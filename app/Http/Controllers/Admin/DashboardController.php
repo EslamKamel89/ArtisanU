f2e192b1865727e529508fb93e2c7e4331b0d4aa
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-29 13:22:19 +0300

    fix(layout): update asset loading - Replace direct paths with Laravel asset() helper

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller {
-    public function index() {
-        return view('admin.dashboard');
+    public function index(): View {
+        return view('admin.layouts.master');
+        // return view('admin.dashboard');
     }
 }
